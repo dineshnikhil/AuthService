465ca269608e8d6d6b0fad64e255efa8d033e759
dineshnikhil <dineshkumarpokkula@gmail.com>
2023-07-12 10:11:14 +0530

    added the jwt token creation and validation in the services layer.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { PORT } = require('./config/serverConfig');
+require('dotenv').config();
+
+const { PORT, JWT_KEY } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-const { UserRepository } = require('./repository/index');
+const { UserServices } = require('./services/index');
 
 const createAndProcessServer = () => {
 	const app = express();
@@ -17,9 +19,13 @@ const createAndProcessServer = () => {
 	app.listen(PORT, async () => {
 		console.log('Server is started on port', PORT);
 
-		const userRepository = new UserRepository();
-		const user = await userRepository.getUserById(1);
-		console.log(user);
+		const userServices = new UserServices();
+		// const token = userServices.createToken({ email: 'dinesh@gmail', id: 2 });
+		// console.log(token);
+		const result = userServices.validateToken(
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRpbmVzaEBnbWFpbCIsImlkIjoyLCJpYXQiOjE2ODkxMzY3MTMsImV4cCI6MTY4OTE0MDMxM30.ezGtbQ_SZY9n9cGE5AOSSiHLUxIkWVHsH_QQBk4p2mc'
+		);
+		console.log(result);
 	});
 };
 
--- src/services/user-services.js
+++ src/services/user-services.js
@@ -1,4 +1,7 @@
 const { UserRepository } = require('../repository/index');
+const jwt = require('jsonwebtoken');
+
+const { JWT_KEY } = require('../config/serverConfig');
 
 class UserServices {
 	constructor() {
@@ -11,6 +14,26 @@ class UserServices {
 			return user;
 		} catch (error) {
 			console.log('Something went wrong in service layer..!');
+			throw { error };
+		}
+	}
+
+	createToken(user) {
+		try {
+			const token = jwt.sign(user, JWT_KEY, { expiresIn: '1h' });
+			return token;
+		} catch (error) {
+			console.log('Something went wrong in while creating the token..!');
+			throw { error };
+		}
+	}
+
+	validateToken(token) {
+		try {
+			const validity = jwt.verify(token, JWT_KEY);
+			return validity;
+		} catch (error) {
+			console.log('Something went wrong in while validating the token..!');
 			throw { error };
 		}
 	}
